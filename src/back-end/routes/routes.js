e49981e19fc8b5893777add8394bdba2a3d38fa0
FelipeGodoi12 <142548396+FelipeGodoi12@users.noreply.github.com>
2025-11-01 19:47:07 -0300

    Add files via upload

--- src/back-end/routes/routes.js
+++ src/back-end/routes/routes.js
@@ -5,23 +5,23 @@ const path = require('path')
 
 //Página inicial
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../front-end/Páginas/Homepage.html'))
+  res.sendFile(path.resolve(__dirname, '../../front-end/Páginas/homepage.html'))
 })
 
 router.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'front-end', 'Login.html'));
-});
+  res.sendFile(path.resolve(__dirname, '../../front-end/Páginas/login.html'))
+})
 
 router.get('/cadastro', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'front-end', 'cadastro.html'));
+  res.sendFile(path.resolve(__dirname, '../../front-end/Páginas/cadastro.html'))
 });
 
 router.get('/oficinas', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'front-end', 'Oficinas.html'));
+  res.sendFile(path.resolve(__dirname, '../../front-end/Páginas/oficinas.html'))
 });
 
 router.get('/criar-oficinas', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'front-end', 'CriarOficinas.html'));
+  res.sendFile(path.resolve(__dirname, '../../front-end/Páginas/criar-oficinas.html'))
 });
 
 module.exports = router
