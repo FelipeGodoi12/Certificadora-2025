c40cea8cdac0fc26ad8f53e67a93fa2bcd5e3a33
Matheus-Barreto06 <164064946+Matheus-Barreto06@users.noreply.github.com>
2025-11-14 10:34:20 -0300

    Update users.js

--- src/back-end/routes/users.js
+++ src/back-end/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User.js')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt') 
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const SECRET = process.env.SECRET
@@ -18,7 +18,9 @@ router.post('/cadastro', async (req, res) => {
         if (existente) {
             return res.status(400).send({ erro: 'Este email já foi cadastrado' });
         }
-        const novoUsuario = new User({ nome, email, password })
+        
+        
+        const novoUsuario = new User({ nome, email, password }) 
         await novoUsuario.save()
         res.status(201).send({ message: 'Usuário cadastrado com sucesso.' })
     } catch (error) {
@@ -36,14 +38,19 @@ router.post('/login', async (req, res) => {
     if (!usuario) {
       return res.status(401).json({ erro: 'Usuário não encontrado.' })
     }
+
+    
     const senhaConfere = password === usuario.password 
     if (!senhaConfere) {
       return res.status(401).json({ erro: 'Senha incorreta.' })
     }
 
+ 
     const token = jwt.sign(
-      { id: usuario._id, email: usuario.email }, SECRET, { expiresIn: '24h' }
+      { id: usuario._id, email: usuario.email, admin: usuario.admin }, 
+      SECRET, { expiresIn: '24h' }
     )
+    
     console.log(usuario.email)
     console.log(usuario.admin)
     return res.json({ token, admin: usuario.admin,
@@ -55,8 +62,5 @@ router.post('/login', async (req, res) => {
     return res.status(500).json({ erro: 'Erro interno no servidor.' })
   }
 })
-
-
-
 
 module.exports = router
