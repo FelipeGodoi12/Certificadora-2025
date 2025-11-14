b8d22ed81ca751b64317347508a1318743f81dbb
FelipeGodoi12 <142548396+FelipeGodoi12@users.noreply.github.com>
2025-11-14 12:40:01 -0300

    Add files via upload

--- src/back-end/models/User.js
+++ src/back-end/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     nome: { type: String, required: true }, 
-    email: { type: String, required: true, unique: true},
+    email: { type: String, required: true },
     password: { type: String, required: true },
     admin: {type: Boolean, required: false, default: false },
     createdAt: { type: Date, default: Date.now }
--- src/back-end/routes/users.js
+++ src/back-end/routes/users.js
@@ -1,26 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User.js')
-const bcrypt = require('bcrypt') 
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
-const SECRET = process.env.SECRET
+//const SECRET = process.env.SECRET
+SECRET = 'pacienciafelipe:)'
+
 
 // Cadastro 
 router.post('/cadastro', async (req, res) => {
     console.log(req.body) 
     try {
         const { nome, email, password } = req.body
-         if (!password || password.length < 6 || password.length > 12) {
-            return res.status(400).send({ erro: 'A senha deve ter entre 6 e 12 caracteres.' });
-        }
-        const existente = await User.findOne({ email });
-        if (existente) {
-            return res.status(400).send({ erro: 'Este email já foi cadastrado' });
-        }
-        
-        
-        const novoUsuario = new User({ nome, email, password }) 
+        const novoUsuario = new User({ nome, email, password })
         await novoUsuario.save()
         res.status(201).send({ message: 'Usuário cadastrado com sucesso.' })
     } catch (error) {
@@ -38,19 +31,14 @@ router.post('/login', async (req, res) => {
     if (!usuario) {
       return res.status(401).json({ erro: 'Usuário não encontrado.' })
     }
-
-    
     const senhaConfere = password === usuario.password 
     if (!senhaConfere) {
       return res.status(401).json({ erro: 'Senha incorreta.' })
     }
 
- 
     const token = jwt.sign(
-      { id: usuario._id, email: usuario.email, admin: usuario.admin }, 
-      SECRET, { expiresIn: '24h' }
+      { id: usuario._id, email: usuario.email }, SECRET, { expiresIn: '24h' }
     )
-    
     console.log(usuario.email)
     console.log(usuario.admin)
     return res.json({ token, admin: usuario.admin,
@@ -62,5 +50,8 @@ router.post('/login', async (req, res) => {
     return res.status(500).json({ erro: 'Erro interno no servidor.' })
   }
 })
+
+
+
 
 module.exports = router
